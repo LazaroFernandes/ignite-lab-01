131ebd52af621fbfb965bcda825e82f23f000180
lazarofernandes <lazarofernandes240@gmail.com>
2022-05-24 12:31:27 -0300

    Eighth Commit

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,12 +1,17 @@
+import { gql, useQuery } from '@apollo/client';
 import { getAccessToken, useUser, withPageAuthRequired } from '@auth0/nextjs-auth0'
+import { withApollo } from '../../lib/withApollo';
 
-
-export default function Home() {
+function Home() {
     const { user } = useUser()
+    const { data, loading, error } = useQuery(PRODUCTS_QUERY)
 
     return (
         <>
             <h1>Hello World</h1>
+            <pre>
+                {JSON.stringify(data, null, 2)}
+            </pre>
             <pre>
                 {JSON.stringify(user, null, 2)}
             </pre>
@@ -16,8 +21,8 @@ export default function Home() {
 
 export const getServerSideProps = withPageAuthRequired({
     getServerSideProps: async ({ req, res }) => {
-        console.log(getAccessToken(req, res))
-        
+        // console.log(getAccessToken(req, res))
+
         return {
             props: {
 
@@ -25,3 +30,6 @@ export const getServerSideProps = withPageAuthRequired({
         }
     }
 });
+
+
+export default withApollo(Home);
